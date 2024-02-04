6d40bb59b49ad994a37d2365e0ca1cab05950bee
ph <chenyi.ph@outlook.com>
2024-02-04 14:16:44 +0800

    set three version

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
 			external: ["three"],
 			output: {
 				paths: {
-					three: "https://unpkg.com/three@<version>/build/three.module.js",
+					three: "https://unpkg.com/three@0.161.0/build/three.module.js",
 				},
 			},
 		},
