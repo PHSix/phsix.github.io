7fdd8efcf238bd37e5ce7936a39d29fe9b938b24
ph <chenyi.ph@outlook.com>
2024-02-04 14:13:17 +0800

    use cdn for threejs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-  server: {
-    port: 3000
-  }
-})
+	plugins: [preact()],
+	server: {
+		port: 3000,
+	},
+	build: {
+		rollupOptions: {
+			external: ["three"],
+			output: {
+				paths: {
+					three: "https://unpkg.com/three@<version>/build/three.module.js",
+				},
+			},
+		},
+	},
+});
