5dd9cc9c3defe8e43bc487e76f0a4c5564191fdc
ph <chenyi.ph@outlook.com>
2024-07-19 09:54:39 +0800

    fix dark

--- src/hooks/useDark.ts
+++ src/hooks/useDark.ts
@@ -1,10 +1,23 @@
 'use client'
 import { useEffect, useState } from 'react'
 
+function getFromSession(): boolean | null {
+  const item = sessionStorage.getItem('--is-dark')
+  if (!item)
+    return null
+  try {
+    const val = Boolean(item)
+    return val
+  } catch {
+    return null
+  }
+}
+
 export default function useDark(): [boolean, (value: boolean) => void] {
-  const [dark, _setDark] = useState(() => typeof window === 'undefined' ? false : window.matchMedia('(prefers-color-scheme: dark)').matches)
+  const [dark, _setDark] = useState(() => typeof window === 'undefined' ? false : (getFromSession() ?? window.matchMedia('(prefers-color-scheme: dark)').matches))
   function setDark(val: boolean) {
     _setDark(val)
+    sessionStorage.setItem('--is-dark', `${val}`)
     if (val)
       document.documentElement.classList.add('dark')
     else
